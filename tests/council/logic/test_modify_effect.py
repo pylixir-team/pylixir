932409b55a21f5382c5f588ee45912cbcc1d5ca6
oleneyl <meson3241@gmail.com>
2023-03-12 17:38:57 +0900

    feat: check target count exception

--- tests/council/logic/test_modify_effect.py
+++ tests/council/logic/test_modify_effect.py
@@ -1,7 +1,7 @@
 import pytest
 
 from pylixir.core.base import GameState
-from pylixir.council.logic import IncreaseTargetWithRatio
+from pylixir.council.logic import IncreaseTargetWithRatio, TargetSizeMismatchException
 from tests.council.logic.util import assert_effect_changed
 
 
@@ -29,3 +29,12 @@ def test_increase_target_with_ratio(
     assert_effect_changed(
         abundant_state, changed_state, target_index, amount if success else 0
     )
+
+
+def test_increase_target_with_ratio_reject_multiple_target(abundant_state):
+    with pytest.raises(TargetSizeMismatchException):
+        IncreaseTargetWithRatio(
+            ratio=2500,
+            value=(1, 0),
+            remain_turn=1,
+        ).reduce(abundant_state, [2, 3], 2344)
