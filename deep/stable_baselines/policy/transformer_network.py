b111c11f5686765c9ac478991189f7c2d94b8396
oleneyl <39217532+oleneyl@users.noreply.github.com>
2023-07-22 20:52:45 +0900

    feat: runnable

--- deep/stable_baselines/policy/transformer_network.py
+++ deep/stable_baselines/policy/transformer_network.py
@@ -7,6 +7,7 @@ from torch import nn
 from stable_baselines3.common.policies import BasePolicy
 from stable_baselines3.common.torch_layers import (
     BaseFeaturesExtractor,
+    FlattenExtractor,
 )
 from stable_baselines3.common.type_aliases import Schedule
 from stable_baselines3.dqn.policies import DQNPolicy
@@ -110,8 +111,6 @@ class TransformerQNetwork(BasePolicy):
 
     :param observation_space: Observation space
     :param action_space: Action space
-    :param net_arch: The specification of the policy and value networks.
-    :param activation_fn: Activation function
     :param normalize_images: Whether to normalize images or not,
          dividing by 255.0 (True by default)
     """
@@ -135,7 +134,6 @@ class TransformerQNetwork(BasePolicy):
             features_extractor=features_extractor,
             normalize_images=normalize_images,
         )
-
 
         self.vector_size = vector_size
         self.hidden_dimension = hidden_dimension
@@ -163,9 +161,128 @@ class TransformerQNetwork(BasePolicy):
         )
         return data
 
+    def forward(self, obs: th.Tensor) -> th.Tensor:
+        """
+        Predict the q-values.
 
-class TransformerQPolicy(DQNPolicy):
+        :param obs: Observation
+        :return: The estimated Q-Value for each action.
+        """
+        return self.q_net(self.extract_features(obs, self.features_extractor))
+
+    def _predict(self, observation: th.Tensor, deterministic: bool = True) -> th.Tensor:
+        q_values = self(observation)
+        # Greedy action
+        action = q_values.argmax(dim=1).reshape(-1)
+        return action
+
+
+class TransformerQPolicy(BasePolicy):
+
+    q_net: TransformerQNetwork
+    q_net_target: TransformerQNetwork
+
+    def __init__(
+        self,
+        observation_space: spaces.Space,
+        action_space: spaces.Discrete,
+        lr_schedule: Schedule,
+        features_extractor_class: Type[BaseFeaturesExtractor] = FlattenExtractor,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[th.optim.Optimizer] = th.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        vector_size: int = 128, 
+        hidden_dimension: int = 64,
+        transformer_layers: int = 3,
+        transformer_heads: int = 8,
+    ) -> None:
+        super().__init__(
+            observation_space,
+            action_space,
+            features_extractor_class,
+            features_extractor_kwargs,
+            optimizer_class=optimizer_class,
+            optimizer_kwargs=optimizer_kwargs,
+            normalize_images=normalize_images,
+        )
+
+        self.vector_size = vector_size
+        self.hidden_dimension = hidden_dimension
+        self.transformer_layers = transformer_layers
+        self.transformer_heads = transformer_heads
+
+        self.net_args = {
+            "observation_space": self.observation_space,
+            "action_space": self.action_space,
+            "vector_size": vector_size,
+            "hidden_dimension": hidden_dimension,
+            "transformer_layers": transformer_layers,
+            "transformer_heads": transformer_heads,
+            "normalize_images": normalize_images,
+        }
+
+        self._build(lr_schedule)
+
+    def _build(self, lr_schedule: Schedule) -> None:
+        """
+        Create the network and the optimizer.
+
+        Put the target network into evaluation mode.
+
+        :param lr_schedule: Learning rate schedule
+            lr_schedule(1) is the initial learning rate
+        """
+
+        self.q_net = self.make_q_net()
+        self.q_net_target = self.make_q_net()
+        self.q_net_target.load_state_dict(self.q_net.state_dict())
+        self.q_net_target.set_training_mode(False)
+
+        # Setup optimizer with initial learning rate
+        self.optimizer = self.optimizer_class(  # type: ignore[call-arg]
+            self.parameters(),
+            lr=lr_schedule(1),
+            **self.optimizer_kwargs,
+        )
+
     def make_q_net(self) -> TransformerQNetwork:
         # Make sure we always have separate networks for features extractors etc
         net_args = self._update_features_extractor(self.net_args, features_extractor=None)
+        net_args.pop("features_dim")
         return TransformerQNetwork(**net_args).to(self.device)
+
+    def forward(self, obs: th.Tensor, deterministic: bool = True) -> th.Tensor:
+        return self._predict(obs, deterministic=deterministic)
+
+    def _predict(self, obs: th.Tensor, deterministic: bool = True) -> th.Tensor:
+        return self.q_net._predict(obs, deterministic=deterministic)
+
+    def _get_constructor_parameters(self) -> Dict[str, Any]:
+        data = super()._get_constructor_parameters()
+
+        data.update(
+            dict(
+                vector_size=self.net_args["vector_size"],
+                hidden_dimension=self.net_args["hidden_dimension"],
+                transformer_layers=self.net_args["transformer_layers"],
+                transformer_heads=self.net_args["transformer_heads"],
+                lr_schedule=self._dummy_schedule,  # dummy lr schedule, not needed for loading policy alone
+                optimizer_class=self.optimizer_class,
+                optimizer_kwargs=self.optimizer_kwargs,
+                features_extractor_class=self.features_extractor_class,
+                features_extractor_kwargs=self.features_extractor_kwargs,
+            )
+        )
+        return data
+
+    def set_training_mode(self, mode: bool) -> None:
+        """
+        Put the policy in either training or evaluation mode.
+
+        This affects certain modules, such as batch normalisation and dropout.
+
+        :param mode: if true, set to training mode, else set to evaluation mode
+        """
+        self.q_net.set_training_mode(mode)
+        self.training = mode
