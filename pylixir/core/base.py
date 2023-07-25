257364ce3c505b3d313c2735dfa6fffa40e46889
MilkClouds <milkclouds00@gmail.com>
2023-07-25 13:17:14 +0900

    fix: set maximum effect count from 11to10

--- pylixir/core/base.py
+++ pylixir/core/base.py
@@ -84,7 +84,7 @@ class Effect(pydantic.BaseModel, metaclass=abc.ABCMeta):
         return not self.locked and self.value < self.max_value
 
 
-MAX_EFFECT_COUNT = 11
+MAX_EFFECT_COUNT = 10
 
 
 class Board(pydantic.BaseModel):
